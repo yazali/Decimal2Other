dbbab5ab33179ac2b24bb76f73a67530f3d99e99
yazali <ravikumar.yazali@tavant.com>
2015-11-25 12:05:40 +0530

    Update dec2bin.rb

--- dec2bin.rb
+++ dec2bin.rb
@@ -1,2 +1,12 @@
 class Dect0Bin
+  
+  attr_accessor :number
+  
+  def getInput()
+     puts "enter any decimal number in numeric format"
+     @number = gets.chomp.to_i
+     puts "binary equivalent is #{@number.to_s(2)}"
+  end
 end
+
+Dect0Bin.new.getInput()
